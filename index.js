b4a67cf3fe9b9ef7d1b8d0acf7a0a23d4d178982
Reinald Mendes <reinald.2967@aluno.pr.senac.br>
2024-06-25 08:17:26 -0300

    init:  anagrama

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-console.log("digite uma palavra");
 process.stdin.on("data", function (data) {
-  let input = data.toString().trim();
+  const word = data.toString().trim();
+
+  let anagrams = [""];
+  for (let char of word) {
+    let tempAnagrams = [];
+    for (let anagram of anagrams) {
+      for (let i = 0; i <= anagram.length; i++) {
+        tempAnagrams.push(anagram.slice(0, i) + char + anagram.slice(i));
+      }
+    }
+    anagrams = tempAnagrams;
+  }
+
+  console.log(`Anagramas de "${word}":`, anagrams);
+  process.exit();
 });
+
+console.log("Digite uma palavra: ");
